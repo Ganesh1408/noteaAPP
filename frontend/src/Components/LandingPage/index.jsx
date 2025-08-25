339a41f4f5388a7b449958e46aa56a46576e03f2
Ganesh1408 <121733704+Ganesh1408@users.noreply.github.com>
2025-08-25 13:39:04 +0530

    add assets

--- frontend/src/Components/LandingPage/index.jsx
+++ frontend/src/Components/LandingPage/index.jsx
@@ -7,7 +7,7 @@ function LandingPage(){
         return(
             <>
             <div className="w-full h-full flex flex-col justify-center items-center mt-6 ">
-            <img src="/public/assets/spot_balls.png" alt='spot_balls'/>
+            <img src="/assets/spot_balls.png" alt='spot_balls'/>
            
             </div>
             <div className=" m-auto p-6 text-center ">
@@ -15,7 +15,7 @@ function LandingPage(){
              <p className=" text-xl font-mono md:text-2xl  font-medium mt-3  ">A minimalist Notes app which helps you manage your time and be productive, the monochrome design avoids it from being destracting</p>
            
             <Link to="/home" ><button  className=' mx-auto text-xl md:text-2xl w-[230px] h-[44px] md:w-[253px] md:h-[58px] mt-4 bg-black text-white rounded-3xl  '  >Let's start</button></Link>
-             <img src="/public/assets/Logo.png" alt="logo" className="  mt-4 m-auto w-[50px] h-[14px]  md:w-[61px] md:h-[17px] "/>
+             <img src="/assets/Logo.png" alt="logo" className="  mt-4 m-auto w-[50px] h-[14px]  md:w-[61px] md:h-[17px] "/>
              
              </div>
              </>
